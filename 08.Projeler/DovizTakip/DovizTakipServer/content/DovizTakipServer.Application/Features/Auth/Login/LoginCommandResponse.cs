a5a99979d5d046bae02f0fb8d638cf8c25f43889
TanerSaydam <tanersaydam@gmail.com>
2024-04-21 11:33:27 +0300

    21.04.2024 Ders arası düzeltmesi

--- 08.Projeler/DovizTakip/DovizTakipServer/content/DovizTakipServer.Application/Features/Auth/Login/LoginCommandResponse.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DovizTakipServer.Application.Features.Auth.Login;
-
-public sealed record LoginCommandResponse(
-    string Token,
-    string RefreshToken,
-    DateTime RefreshTokenExpires);
--- 08.Projeler/DovizTakip/DovizTakipServer/content/DovizTakipServer.Infrastructure/Options/JwtOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DovizTakipServer.Infrastructure.Options;
-public sealed class JwtOptions
-{
-    public string Issuer { get; set; } = string.Empty;
-    public string Audience { get; set; } = string.Empty;
-    public string SecretKey { get; set; } = string.Empty;
-}
